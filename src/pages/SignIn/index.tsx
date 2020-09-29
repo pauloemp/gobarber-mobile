c69f2bfa32b429d2beb0ee060d62b7a119adc55a
Paulo Pacheco <paulo95.emp@gmail.com>
2020-09-28 21:23:09 -0300

    Config navigator

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const SignIn: React.FC = () => {
+  return <S.Container />;
+};
+
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const SignUp: React.FC = () => {
+  return <S.Container />;
+};
+
+export default SignUp;

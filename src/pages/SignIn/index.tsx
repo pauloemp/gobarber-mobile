0a103a73a7c0861312ce04d6522f5943e30aca29
Paulo Pacheco <paulo95.emp@gmail.com>
2020-09-28 21:29:34 -0300

    Import images and pixel density

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import * as S from './styles';
 
+import logoImg from '../../assets/images/logo.png';
+
 const SignIn: React.FC = () => {
-  return <S.Container />;
+  return (
+    <S.Container>
+      <Image source={logoImg} />
+    </S.Container>
+  );
 };
 
 export default SignIn;

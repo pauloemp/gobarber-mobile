b853fe49e340fcacd42d261df5bbf58ea91145c4
Paulo Pacheco <paulo95.emp@gmail.com>
2020-10-19 17:04:19 -0300

    Add private routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -21,6 +21,7 @@ interface SignInCredentials {
 
 interface IAuthContext {
   user: AuthState['user'];
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -29,6 +30,7 @@ const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadStorageData = async (): Promise<void> => {
@@ -38,6 +40,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token && user) setData({ token, user: JSON.parse(user) });
+
+      setLoading(false);
     };
 
     loadStorageData();
@@ -66,7 +70,7 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
